084106b908c1df3e0037e1b7acad5070f77a199b
Anas Hameed <68567262+Anas-hameed@users.noreply.github.com>
2023-06-11 12:11:15 +0500

    Update axiosinstance.tsx

--- src/axios/axiosinstance.tsx
+++ src/axios/axiosinstance.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = "http://localhost:4000/digsync/api/v0.1";
+const baseURL = "http://15.206.179.59:4000/digsync/api/v0.1";
 
 var loginToken = localStorage.getItem('token');
 
